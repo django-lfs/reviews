33edd7863d9154717e83384327e6eabc1dd25857
kai.diefenbach@iqpp.de <kai.diefenbach@iqpp.de@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-06-30 19:29:37 +0000

    Return additonally amount of ratings.
    git-svn-id: http://django-lfs.googlecode.com/svn/addons/reviews/trunk@508 70813176-cce2-11dd-87b6-9d1a99559f42

--- utils.py
+++ utils.py
@@ -11,36 +11,36 @@ def get_reviews_for_instance(instance):
     """
     ctype = ContentType.objects.get_for_model(instance)
     return Review.objects.active().filter(content_type=ctype.id, content_id=instance.id)
-    
+
 def get_average_for_instance(instance):
-    """Returns the average score of the given instance. 
-    
+    """Returns the average score of the given instance.
+
     Takes only active reviews into account.
     """
     # TODO: Check Django 1.1's aggregation
     ctype = ContentType.objects.get_for_model(instance)
     cursor = connection.cursor()
-    cursor.execute("""SELECT avg(score) 
+    cursor.execute("""SELECT avg(score), count(*)
                       FROM reviews_review
-                      WHERE content_type_id=%s 
+                      WHERE content_type_id=%s
                       AND content_id=%s
                       AND active=%s""", [ctype.id, instance.id, True])
 
-    return cursor.fetchone()[0]
+    return cursor.fetchone()
 
 def has_rated(request, instance):
-    """Returns True if the current user has already rated for the given 
+    """Returns True if the current user has already rated for the given
     instance.
     """
     session_key = request.session.session_key
     ctype = ContentType.objects.get_for_model(instance)
-        
+
     try:
         if request.user.is_authenticated():
             review = Review.objects.get(content_type=ctype.id,
                 content_id=instance.id, user=request.user)
         else:
-            review = Review.objects.get(content_type=ctype.id, 
+            review = Review.objects.get(content_type=ctype.id,
                 content_id=instance.id, session_id=request.session.session_key)
     except ObjectDoesNotExist:
         return False
