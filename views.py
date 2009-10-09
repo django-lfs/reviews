1870533cce00ede5ac8c7b9df39bdb1118ea835b
kai.diefenbach@gmail.com <kai.diefenbach@gmail.com@70813176-cce2-11dd-87b6-9d1a99559f42>
2009-10-09 17:36:40 +0000

    Update review templates to new settings. Display links to shop and rated product.
    git-svn-id: http://django-lfs.googlecode.com/svn/addons/reviews/trunk@911 70813176-cce2-11dd-87b6-9d1a99559f42

--- views.py
+++ views.py
@@ -1,4 +1,5 @@
 # django imports
+from django.conf import settings
 from django.contrib.contenttypes.models import ContentType
 from django.core.urlresolvers import reverse
 from django.forms import ModelForm
@@ -19,28 +20,33 @@ class ReviewAddForm(ModelForm):
     class Meta:
         model = Review
         fields = ("user_name", "user_email", "comment", "score")
-    
+
     def clean(self):
         """
         """
-        # For an anonymous user the name is required. Please note that the 
+        # For an anonymous user the name is required. Please note that the
         # request has to be passed explicitely to the form object (see add_form)
         msg = _(u"This field is required")
-        if self.request.user.is_anonymous():
-            if self.cleaned_data.get("user_name", "") == "":
-                self._errors["user_name"] = ErrorList([msg])
 
-        return self.cleaned_data 
-        
+        if self.request.user.is_anonymous():
+            if settings.REVIEWS_IS_NAME_REQUIRED:
+                if self.cleaned_data.get("user_name", "") == "":
+                    self._errors["user_name"] = ErrorList([msg])
+            if settings.REVIEWS_IS_EMAIL_REQUIRED:
+                if self.cleaned_data.get("user_email", "") == "":
+                    self._errors["user_email"] = ErrorList([msg])
+
+        return self.cleaned_data
+
 def add_form(request, content_type_id, content_id, template_name="reviews/review_form.html"):
     """Displays the form to add a review.
     """
     ctype = ContentType.objects.get_for_id(content_type_id)
     object = ctype.get_object_for_this_type(pk=content_id)
-    
+
     if reviews_utils.has_rated(request, object):
         return HttpResponseRedirect(reverse("reviews_already_rated"))
-        
+
     scores = []
     for i, score in enumerate(SCORE_CHOICES):
         scores.append({
@@ -49,35 +55,38 @@ def add_form(request, content_type_id, content_id, template_name="reviews/review
             "z_index" : 10-i,
             "width" : (i+1) * 25,
         })
-    
+
     if request.method == "POST":
         form = ReviewAddForm(data=request.POST)
         form.request = request
         if form.is_valid():
-            return preview(request)
+            if settings.REVIEWS_SHOW_PREVIEW:
+                return preview(request)
+            else:
+                return save(request)
     else:
         form = ReviewAddForm()
-        
-        
+
     return render_to_response(template_name, RequestContext(request, {
         "content_type_id" : content_type_id,
         "content_id" : content_id,
         "object" : object,
         "form" : form,
         "scores" : scores,
+        "show_preview" : settings.REVIEWS_SHOW_PREVIEW,
     }))
 
 def reedit(request, template_name="reviews/review_form.html"):
-    """Displays a form to edit a review. This is used if a reviewer re-edits 
+    """Displays a form to edit a review. This is used if a reviewer re-edits
     a review after she has previewed it.
     """
-    # get object    
+    # get object
     content_type_id = request.POST.get("content_type_id")
     content_id = request.POST.get("content_id")
 
     ctype = ContentType.objects.get_for_id(content_type_id)
     object = ctype.get_object_for_this_type(pk=content_id)
-    
+
     if reviews_utils.has_rated(request, object):
         return HttpResponseRedirect(reverse("reviews_already_rated"))
 
@@ -90,7 +99,7 @@ def reedit(request, template_name="reviews/review_form.html"):
             "z_index" : 10-i,
             "width" : (i+1) * 25,
         })
-    
+
     form = ReviewAddForm(data=request.POST)
     return render_to_response(template_name, RequestContext(request, {
         "content_type_id" : content_type_id,
@@ -98,56 +107,77 @@ def reedit(request, template_name="reviews/review_form.html"):
         "form" : form,
         "scores" : scores,
         "object" : object,
+        "show_preview" : settings.REVIEWS_SHOW_PREVIEW,
     }))
-    
+
 def reedit_or_save(request):
     """Edits or saves a review dependend on which button has been pressed.
     """
     if request.POST.get("edit"):
         return reedit(request)
     else:
-        form = ReviewAddForm(data=request.POST)
-        form.request = request
-        if form.is_valid():
-            new_review = form.save(commit=False)
-            new_review.content_type_id = request.POST.get("content_type_id")
-            new_review.content_id = request.POST.get("content_id")
-            new_review.session_id = request.session.session_key
-            new_review.ip_address = request.META.get("REMOTE_ADDR")
-            if request.user.is_authenticated():
-                new_review.user = request.user
-            new_review.save()
+        return save(request)
+
+def save(request):
+    """Saves a review.
+    """
+    form = ReviewAddForm(data=request.POST)
+    form.request = request
+    if form.is_valid():
+        new_review = form.save(commit=False)
+        new_review.content_type_id = request.POST.get("content_type_id")
+        new_review.content_id = request.POST.get("content_id")
+        new_review.session_id = request.session.session_key
+        new_review.ip_address = request.META.get("REMOTE_ADDR")
+        if request.user.is_authenticated():
+            new_review.user = request.user
+        new_review.active = not settings.REVIEWS_IS_MODERATED
+        new_review.save()
+        
+        # Save object within session
+        ctype = ContentType.objects.get_for_id(new_review.content_type_id)
+        object = ctype.get_object_for_this_type(pk=new_review.content_id)        
+        request.session["last-rated-object"] = object
+        
         return HttpResponseRedirect(reverse("reviews_thank_you"))
-    
+
 def preview(request, template_name="reviews/review_preview.html"):
     """Displays a preview of the review.
     """
     content_type_id = request.POST.get("content_type_id")
     content_id = request.POST.get("content_id")
-    
+
     ctype = ContentType.objects.get_for_id(content_type_id)
     object = ctype.get_object_for_this_type(pk=content_id)
-    
+
     if request.user.is_authenticated():
         name = request.user.get_full_name()
         email = request.user.email
     else:
         name = request.POST.get("user_name")
         email = request.POST.get("user_email")
-                
+
     return render_to_response(template_name, RequestContext(request, {
         "score" : float(request.POST.get("score", 0)),
         "object" : object,
         "name" : name,
         "email" : email,
     }))
-    
+
 def thank_you(request, template_name="reviews/thank_you.html"):
     """Displays a thank you page.
     """
-    return render_to_response(template_name, RequestContext(request))
-    
+    if request.session.has_key("last-rated-object"):
+        object = request.session.get("last-rated-object")
+        del request.session["last-rated-object"]
+    else:
+        object = None
+
+    return render_to_response(template_name, RequestContext(request, {
+        "object" : object,
+    }))
+
 def already_rated(request, template_name="reviews/already_rated.html"):
-    """
+    """Displays a alreday rated page.
     """
     return render_to_response(template_name, RequestContext(request))
